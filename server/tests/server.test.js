70b910fcb28df567a7647bc631950bd5b01fe81c
PoeticPete <petertao95@gmail.com>
2017-08-23 18:00:36 -0400

    added GET tests for users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {User} = require('./../models/user');
 
+const users = [{
+  device_id: 'some device id'
+}, {
+  device_id: 'some other device id'
+}]
+
 beforeEach((done) => {
-  User.remove({}).then(() => done());
-})
+  User.remove({}).then(() => {
+    return User.insertMany(users);
+  }).then(() => done());
+});
 
 describe('Post /users', () => {
   it('should create a new user with device_id', (done) => {
@@ -25,8 +33,7 @@ describe('Post /users', () => {
         }
 
         User.find().then((users) => {
-          expect(users.length).toBe(1);
-          expect(users[0].device_id).toBe(device_id);
+          expect(users.length).toBe(3);
           done()
         }).catch((e) => done(e))
       })
@@ -44,9 +51,21 @@ describe('Post /users', () => {
         }
 
         User.find().then((users) => {
-          expect(users.length).toBe(0);
+          expect(users.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /users', () => {
+  it('should get all users', (done) => {
+    request(app)
+      .get('/users')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.users.length).toBe(2);
+      })
+      .end(done);
   });
 });
