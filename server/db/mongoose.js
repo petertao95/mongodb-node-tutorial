46aa9751e72c4d9d04012f5521152b61b796b693
PoeticPete <petertao95@gmail.com>
2017-08-23 19:55:36 -0400

    added heroku configurations

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
+mongoose.connect('mongodb://petertao:seastheday@ds157723.mlab.com:57723/breadcrumbs', { useMongoClient: true });
 
 module.exports = {mongoose};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
 
+var {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {User} = require('./../models/user');
 
 const users = [{
+  _id: new ObjectID(),
   device_id: 'some device id'
 }, {
+  _id: new ObjectID(),
   device_id: 'some other device id'
 }]
 
@@ -68,4 +71,32 @@ describe('GET /users', () => {
       })
       .end(done);
   });
+});
+
+describe('GET /users/:id', () => {
+  it('should get a user', (done) => {
+    request(app)
+      .get(`/users/${users[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.user.device_id).toBe(users[0].device_id);
+      })
+      .end(done);
+  });
+
+  it('should return a 404 if user does not exist', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .get(`/users/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return a 404 for non object ids', (done) => {
+    request(app)
+      .get('/users/123')
+      .expect(400)
+      .end(done);
+  });
+
 });
